78243eb53f96a305dc91b43dc653d4059623840d
Dada Khalandar Bellary <kerry.blig12@gmail.com>
2023-06-29 14:08:52 +0530

    added the routers

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         > */}
         <p>
-          Learn React
+          articles
 
         </p>
         {/* </a> */}
--- src/index.tsx
+++ src/index.tsx
@@ -4,30 +4,27 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react'
-
+import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config })
-
-
+const theme = extendTheme({ config });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
