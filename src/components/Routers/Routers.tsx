ba2f8b6d0404140b3de385c24b7f8725f14f2761
Dada Khalandar Bellary <kerry.blig12@gmail.com>
2023-06-29 16:32:19 +0530

    added the home route

--- src/components/Routers/Routers.tsx
+++ src/components/Routers/Routers.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../Home/Home";
 import { Navbar } from "../Navbar/Navbar";
 
 export const Routers = () => {
     return (
         <>
             <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
         </>
     );
 }
